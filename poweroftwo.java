f3507b0e50349ede00c65a90c737be1dc1fed275
Nivetharmm <nivetharmm@gmail.com>
2017-06-22 09:02:04 -0700

    Create poweroftwo.java

---
+++ poweroftwo.java
@@ -0,0 +1,14 @@
+static boolean isPowerOfTwo(int number){
+boolean isPowerOfTwo = true;
+int reminder = 0;
+while(number > 1){
+reminder = number % 2;
+if(reminder != 0){
+isPowerOfTwo = false;
+break;
+}else{
+number = number / 2;
+}
+}
+return isPowerOfTwo;
+}
