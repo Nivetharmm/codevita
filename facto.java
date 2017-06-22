b64824982bc309bcbf8d9cf344fba6642709f0f9
Nivetharmm <nivetharmm@gmail.com>
2017-06-22 08:43:38 -0700

    Create facto.java

---
+++ facto.java
@@ -0,0 +1,12 @@
+
+public class facto {
+
+   public static void main(String[] args) {
+       int n = 7;
+       int result = 1;
+       for (int i = 1; i <= n; i++) {
+           result = result * i;
+       }
+       System.out.println("The factorial of 7 is " + result);
+   }
+}
